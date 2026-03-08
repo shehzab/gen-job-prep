5693d322aa37a7dc07de9035c57bc1a66782cca3
shehzab <sinanshehzab@gmail.com>
2026-03-09 00:17:03 +0530

    UI start

---
+++ frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main>
+      <div className="form-container"></div>
+      <h1>Login</h1>
+      <form action="">
+
+
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id=" email" />
+        </div>
+
+
+      </form>
+    </main>
+  )
+}
+
+export default Login
